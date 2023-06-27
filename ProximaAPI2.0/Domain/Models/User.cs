3a917f89ca690b039bb709a2e6f6a7b80a6a2ed7
Andriy Antonik <59703540+zadrot1995@users.noreply.github.com>
2023-06-27 19:38:07 +0300

    adding user

---
+++ ProximaAPI2.0/Domain/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class User : Entity
+    {
+        public string Login { get; set; }
+        public UserRole UserRole { get; set; }
+        public string Password { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
+    }
+}
